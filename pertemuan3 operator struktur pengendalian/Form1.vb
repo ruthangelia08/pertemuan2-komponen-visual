e16c6f35c52a79a3d3567708287a097bb97a40e5
Nama Anda <ruthangeliasihombing@gmail.com>
2026-09-01 10:23:57 +0700

    Add pertemuan3 operator struktur pengendalian

---
+++ pertemuan3 operator struktur pengendalian/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim nilai As Integer
+
+        If Not Integer.TryParse(txtNilai.Text, nilai) Then
+            MessageBox.Show("Masukkan dalam bentuk angka")
+            Return
+        End If
+        If nilai < 0 OrElse nilai > 100 Then
+            MessageBox.Show("Masukkan Nilai 0 - 100")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilai <= 50 Then
+            picImage.Image = Image.FromFile("asset\1.jpg")
+        ElseIf nilai <= 75 Then
+            picImage.Image = Image.FromFile("asset\2.jpg")
+        Else
+            picImage.Image = Image.FromFile("asset\3.png")
+        End If
+    End Sub
+
+    Private Sub txtNilai_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilai.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub pictImage_Click(sender As Object, e As EventArgs) Handles picImage.Click
+
+    End Sub
+
+    Private Sub txtNilai_TextChanged(sender As Object, e As EventArgs) Handles txtNilai.TextChanged
+
+    End Sub
+End Class
